7703c3ffa79b195a88e77126722b3d7ce4f404bd
ardfar68 <ardfar68@gmail.com>
2024-12-26 22:22:19 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 print("hello world")
 print("hello world_2")
 print("hello world_3")
+def main():
+    """Main function to print hello world messages."""
+    print("hello world")
+    print("hello world_2")
+    print("hello world_3")
+
+
+if __name__ == "__main__":
+    main()
